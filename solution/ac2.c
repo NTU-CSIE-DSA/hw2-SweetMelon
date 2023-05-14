b3a64be8d9b645efa1533d4f48fbfae12abb4874
Alx-Lai <b09902135@csie.ntu.edu.tw>
2023-05-14 15:00:17 +0800

    expand part of macro and eliminate define int long long

--- solution/ac2.c
+++ solution/ac2.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
-#define int long long
+#define ll long long
 #define MAXNM 1000000
 
-// tree
-#define TN(x) (tree[(x)]) // turn id to tree node
-#define NXT_CHD(x) (TN(x).sibling) // get the next child
-#define traverse_chd(x, y) for((y) = TN(x).chd; (y); (y) = NXT_CHD((y))) // traverse all child of a tree node
-
 // heap
-#define TOP(x) (heaps[(x)].heap[1]) // top of the heap
+#define ROOT(x) (heaps[(x)].heap[1]) // root of the heap, one index heap
 #define EMPTY(x) (!heaps[(x)].sz) // whether the heap is empty
 #define SZ(x) (heaps[(x)].sz) // size of heap
-#define LC_ID(x) ((x) << 1) // left child index
-#define RC_ID(x) (((x) << 1) + 1) // right child index
-#define PAR_ID(x) ((x) >> 1) // parent index
+#define LEFT(x) ((x) << 1) // left child index
+#define RIGHT(x) (((x) << 1) + 1) // right child index
+#define PARENT(x) ((x) >> 1) // parent index
 
 typedef struct HeapNode {
-  int cost, exp_day;
+  ll cost, exp_day;
 } HeapNode;
 
 typedef struct Heap {
@@ -26,7 +21,8 @@ typedef struct Heap {
 } Heap;
 
 typedef struct TreeNode {
-  int chd, sibling, hid, sum, cnt;
+  int chd, sibling, hid;
+  ll sum, cnt;
 } TreeNode;
 
 HeapNode h_pool[2 * MAXNM + 1];
@@ -34,31 +30,31 @@ TreeNode tree[MAXNM + 1];
 Heap heaps[MAXNM + 1];
 
 void insert(int par, int n) {
-  TN(n).sibling = TN(par).chd;
-  TN(par).chd = n;
+  tree[n].sibling = tree[par].chd;
+  tree[par].chd = n;
 }
 
 void push_up(HeapNode *heap, int idx) {
-  int par_id = 0;
-  if ((par_id = PAR_ID(idx))) {
-    if (heap[par_id].cost > heap[idx].cost) {
+  int parent = 0;
+  if ((parent = PARENT(idx))) {
+    if (heap[parent].cost > heap[idx].cost) {
       HeapNode tmp = heap[idx];
-      heap[idx] = heap[par_id];
-      heap[par_id] = tmp;
+      heap[idx] = heap[parent];
+      heap[parent] = tmp;
       push_up(heap, idx >> 1);
     }
   }
 }
 
 void push_down(HeapNode *heap, int idx, int sz) {
-  int sml_id = LC_ID(idx) < sz ? LC_ID(idx) : 0; // smallest child
+  int sml_id = LEFT(idx) < sz ? LEFT(idx) : 0; // smallest child
   if (!sml_id) {
     return;
   }
 
-  sml_id = RC_ID(idx) < sz ? RC_ID(idx) : LC_ID(idx);
-  if (heap[sml_id].cost > heap[LC_ID(idx)].cost)
-    sml_id = LC_ID(idx);
+  sml_id = RIGHT(idx) < sz ? RIGHT(idx) : LEFT(idx);
+  if (heap[sml_id].cost > heap[LEFT(idx)].cost)
+    sml_id = LEFT(idx);
   if (heap[sml_id].cost > heap[idx].cost)
     sml_id = idx;
 
@@ -74,12 +70,12 @@ void push_down(HeapNode *heap, int idx, int sz) {
 
 void pop(int hid) {
   HeapNode *heap = heaps[hid].heap;
-  TOP(hid) = heap[--SZ(hid)];
+  ROOT(hid) = heap[--SZ(hid)];
 
   push_down(heap, 1, SZ(hid));
 }
 
-void push(int hid, int c, int d) {
+void push(int hid, ll c, ll d) {
   HeapNode *heap = heaps[hid].heap;
   heap[SZ(hid)++] = (HeapNode){c, d};
 
@@ -87,9 +83,8 @@ void push(int hid, int c, int d) {
 }
 
 void dfs(int n, void(*fn)(int)) {
-  int curr_chd;
-  traverse_chd(n, curr_chd) {
-    dfs(curr_chd, fn);
+  for (int cur = tree[n].chd; cur; cur = tree[cur].sibling) {
+    dfs(cur, fn);
   }
 
   fn(n);
@@ -97,22 +92,20 @@ void dfs(int n, void(*fn)(int)) {
 }
 
 void print_tree(int n) {
-  printf("%lld ", n);
+  printf("%d ", n);
 }
 
 void gen_cnt(int n) {
-  int curr_chd;
-  TN(n).cnt = 1;
-  traverse_chd(n, curr_chd) {
-    TN(n).cnt += TN(curr_chd).cnt;
+  tree[n].cnt = 1;
+  for (int cur = tree[n].chd; cur; cur = tree[cur].sibling) {
+    tree[n].cnt += tree[cur].cnt;
   }
 }
 
 void update(int n) {
-  int curr_chd;
-  TN(n).sum = TOP(TN(n).hid).cost;
-  traverse_chd(n, curr_chd) {
-    TN(n).sum += TN(curr_chd).sum;
+  tree[n].sum = ROOT(tree[n].hid).cost;
+  for (int cur = tree[n].chd; cur; cur = tree[cur].sibling) {
+    tree[n].sum += tree[cur].sum;
   }
 }
 
@@ -120,18 +113,19 @@ void init_heap(int n, int m) {
   for (int i = 1; i <= n; ++i) {
     heaps[i].heap = h_pool + (1 + m) * i;
     heaps[i].sz = 1;
-    TN(i).hid = i;
+    tree[i].hid = i;
   }
 }
 
-signed main () {
-  int n, m, C;
-  scanf("%lld%lld%lld", &n, &m, &C);
+int main () {
+  int n, m;
+  ll C;
+  scanf("%d%d%lld", &n, &m, &C);
 
   init_heap(n, m);
   for (int i = 2; i <= n; ++i) {
     int par;
-    scanf("%lld", &par);
+    scanf("%d", &par);
     insert(par, i);
   }
 
@@ -139,17 +133,17 @@ signed main () {
 
   for (int i = 1; i <= m; ++i) {
     for (int j = 1; j <= n; ++j) {
-      int c, d;
+      ll c, d;
       scanf("%lld%lld", &c, &d);
 
       push(j, c, i + d);
-      while (TOP(j).exp_day < i)
+      while (ROOT(j).exp_day < i)
         pop(j);
     }
     dfs(1, update);
-    int ret = 0;
+    ll ret = 0;
     for (int j = 1; j <= n; ++j)
-      if (TN(j).sum <= C) ret = ret > TN(j).cnt ? ret : TN(j).cnt;
+      if (tree[j].sum <= C) ret = ret > tree[j].cnt ? ret : tree[j].cnt;
 
     printf("%lld\n", ret);
   }
